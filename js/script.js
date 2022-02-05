d79586dfc522713fb62fc7a2231f3b7f7ee7d547
Glombort <george.lambert99@gmail.com>
2022-02-05 02:56:49 +0000

    Fix and Any
    Properly fixed so works
    
    allow any genre closes #10
    
    allows any decade closes #9
    
    allows any language closes #12
    
    allows any runtime closes #11

--- js/script.js
+++ js/script.js
@@ -5,43 +5,56 @@ const filmPromise = fetch("./js/films_list.json")
 
 document.getElementById("pick-btn").onclick = function() {chooserFunc()};
 function chooserFunc() {
-    let userRuntime = document.getElementById('runtime').value;
-    let userGenre = document.getElementById('genre').value;
-    let userLanguage = document.getElementById('language').value;
-    let userDecade = document.getElementById('decade').value;
+    //Sets users choice of film
     var userFilm = {
-        "genre": userGenre,
-        "language": userLanguage,
-        "runtime": userRuntime,
-        "year": userDecade
+        "genre": document.getElementById('genre').value,
+        "language": document.getElementById('language').value,
+        "runtime": document.getElementById('runtime').value,
+        "year": document.getElementById('decade').value
         };
+    //Filters films to get list of all to users requirements 
     let validFilms = filmPromise.then((films) => films.filter(function(item) {
         for (var key in userFilm) {
             if (item[key] === undefined)
                 return false;
             }
-        return runtimeFilter(item['runtime'], userFilm['runtime']) &&
-        runtimeFilter(item['genre'], userFilm['genre']) &&
-        runtimeFilter(item['language'], userFilm['language']) &&
-        runtimeFilter(item['year'], userFilm['year']);
-    })).then(films => {document.getElementById('title-1').innerHTML = films[0].name})
-    
+        return (runtimeFilter(item['runtime'], userFilm['runtime']) && genreFilter(item['genre'], userFilm['genre']) && languageFilter(item['language'], userFilm['language']) && decadeFilter(item['year'], userFilm['year']));}))
+        .then(films => (document.getElementById('title-1').innerHTML=films[0].name))
 }
 
+/*
+Choice filter functions
+*/
 
-function runtimeFilter(filmRun, userRun) {
-    return filmRun <= (userRun + 10);
+//Allows for +10 mins
+function runtimeFilter(filmRuntime, userRuntime) {
+    if (userRuntime == 0) {
+        return true
+    }
+    return filmRuntime <= (parseInt(userRuntime) + 10);
 }
 
-function genreFilter(filmGen, userGen) {
-    return filmGen.includes(userGen);
+//Checks a film contains the users selected genre
+function genreFilter(filmGenre, userGenre) {
+    if (userGenre === 'any-genre') {
+        return true
+    }
+    return filmGenre.includes(userGenre);
 }
 
-function languageFilter(filmLang, userLang) {
-    return filmLang === userLang;
+//Selects language
+function languageFilter(filmLanguage, userLanguage) {
+    if (userLanguage === 'any-lang') {
+        return true
+    }
+    return filmLanguage === userLanguage;
 }
 
-function decadeFilter(filmDec, userDec) {
-    let decade = Math.floor(filmDec.year/10) * 10;
-    return decade === userDec;
+//Selects decade
+function decadeFilter(filmDecade, userDecade) {
+    if (userDecade === 'any-decade') {
+        return true
+    }
+    let decade = Math.floor(filmDecade/10) * 10;
+    return decade === parseInt(userDecade);
 }
